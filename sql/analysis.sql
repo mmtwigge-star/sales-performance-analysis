e1d83f9267663fcff9251e6653016406c574ccb4
mmtwigge-star <mmtwigge@gmail.com>
2026-03-23 12:18:16 +0100

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,1 +1,10 @@
 -- SQL analysis will go here
+
+-- Total monthly sales and profit
+SELECT 
+    DATE_TRUNC('month', "Order Date") AS month,
+    SUM(Sales) AS total_sales,
+    SUM(Profit) AS total_profit
+FROM superstore
+GROUP BY month
+ORDER BY month;
