6973ac183a92b1ef8c3c07989a2c665554662148
Matthew Twigge <mmtwigge@gmail.com>
2026-03-31 09:30:21 +0200

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,7 +1,6 @@
 -- SQL analysis will go here
 
 -- Query 1: Total Sales & Profit by Month
--- Purpose: Aggregate daily order data to see monthly trends
 SELECT 
     substr(order_date, 1, 7) AS month,  -- take YYYY-MM from YYYY-MM-DD
     SUM(Sales) AS total_sales,
